e903ffbb42a238560f080d1772bca478f92302a7
emonhossain151 <ehosan151@gmail.com>
2024-10-03 23:16:52 +0600

    edit

--- bank_management/accounts/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import UserBankAccount, UserAddress
-
-admin.site.register(UserAddress)
-admin.site.register(UserBankAccount)
--- bank_management/accounts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('registration/', views.UserRegistrationView.as_view(), name='registration'),
-]
